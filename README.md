354d5f0f6de642c17e1f2e975a960e703e54dfff
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-04-21 11:56:51 -0400

    Add encapsulation: ViewEncapsulation.Native to encapsulate component styles.

--- README.md
+++ README.md
@@ -20,6 +20,34 @@
 
 ---
 
+```
+.
+├── LICENSE
+├── README.md
+├── config
+├── karma.conf.js
+├── node_modules
+├── package.json
+├── protractor.conf.js
+├── src
+│   ├── app
+│   ├── assets
+│   ├── custom-typings.d.ts
+│   ├── index.html
+│   ├── main.browser.ts
+│   ├── platform
+│   ├── polyfills.ts
+│   └── vendor.ts
+├── tsconfig.json
+├── tslint.json
+├── typedoc.json
+├── typings
+├── typings.json
+└── webpack.config.js
+```
+
+---
+
 ## Observable
 - [Angular 2: HTTP, Observables, and concurrent data loading](http://www.metaltoad.com/blog/angular-2-http-observables)
 - Part of the [JavaScript Reactive Extensions library (RxJS)](https://github.com/Reactive-Extensions/RxJS).
@@ -37,10 +65,25 @@
 - Reactive programming as opposed to imperative programming.
 
 ### Angular2 Pipes
+- Angular2 components have access to pipes.
+- Can be used to transform values via simple expressions.
+- Can be used to connect component data across our application.
 
+### A common way of separating components
+- http://blog.rangle.io/creating-angular-2-style-components-using-angular-1-part-2/
+- Break the design up into dumb and smart components.
+- Using **Observables** and **AsyncPipe** we can set up a bridge of data to flow between our smart and dumb components.
 
+**dumb component**
+- Only present data to the UI view and emit any UI events
 
+**smart component**
+- Concerned with what the dumb components should be displaying.
 
+### Transforming data from http.get to JSON
+- The http.get uses an Observable to subscribe to the incoming network response. - In getting our response, we use map to transform the contents of our data.
+- Our HTTP request only returns a single value, so our Observable stream will only have one element that map will iterate over.
+- We transform this item into an object suitable for consumption by converting the response object into a JSON object, and returning the artists.items property, which contains an array of the artist objects we want to be displayed.
 
 ---
 
@@ -57,9 +100,24 @@
     require('normalize.css'),
     require('app/app.component.css')
    ],
-  templateUrl: 'app/app.component.html'
+  ...
 })
 ```
+
+### [ShadowDOM](http://www.html5rocks.com/en/search?q=Shadow+DOM)
+- Part of the Web Components standard.
+- Enables DOM tree and style encapsulation.
+- Enables us to build a web component with hidden DOM logic under the hood, whose styles only apply to that element.
+
+### How to apply parent's styles to a child component?
+- Apply ViewEncapsulation.Native to parent.
+- Read [View Encapsulation in Angular 2](http://blog.thoughtram.io/angular/2015/06/29/shadow-dom-strategies-in-angular2.html)
+- All our component styles are appended to the document head, but usually would end up in the component’s template in case we use native Shadow DOM.
+- Configure view encapsulation solutions (3 types):
+  + **ViewEncapsulation.None** - No Shadow DOM at all. Unscoped strategy. All the styles apply to the entire document.
+  + **ViewEncapsulation.Emulated** - Default. No Shadow DOM but style encapsulation emulation.
+  + **ViewEncapsulation.Native** - Native Shadow DOM with all it’s goodness.
+
 
 ### Angular material design
 - [mdCard API](https://material.angularjs.org/latest/api/directive/mdCard)
--- src/app/components/spotify-search.component.ts
+++ src/app/components/spotify-search.component.ts
@@ -1,12 +1,18 @@
-import { Component, Output, Input, EventEmitter, Injectable } from 'angular2/core';
+import {
+  Component,
+  Output,
+  Input,
+  EventEmitter,
+  Injectable,
+  ViewEncapsulation } from 'angular2/core';
 import { Http, Response }           from 'angular2/http';
 import { Control, FORM_DIRECTIVES } from 'angular2/common';
 import { Observable }               from 'rxjs/Observable';
 import { Observer }                 from 'rxjs/Observer';
-
+import 'rxjs/Rx';
 
 /**
- * The search box.
+ * The dumb component, search box that captures user input and emits searchEvent.
  */
 @Component({
   selector:   'search-box',
@@ -15,27 +21,46 @@ import { Observer }                 from 'rxjs/Observer';
 })
 class SearchBox {
 
-  @Output() valueChanged: EventEmitter<any> = new EventEmitter();
+  @Output() searchEvent: EventEmitter<any> = new EventEmitter();
 
   private searchBox: Control = new Control();
 
   constructor() {
-    this.searchBox.valueChanges.subscribe( ( event ) =>
-      this.valueChanged.emit( event )
-    );
+    this.searchBox
+      .valueChanges
+      .debounceTime( 200 ) // Control delay. NOTE: import 'rxjs/Rx'
+      .subscribe(
+        ( event ) => this.searchEvent.emit( event )
+      );
   }
 }
 
 
 /**
- * The search result textbox.
+ * The dumb component, search result that accepts search results and displays them.
+ * The result is coming in as an array of artist objects.
+ * The async pipe maintains a subscription to results and keeps delivering values
+ * as they arrive. As long as we push our data through the Observable stream,
+ * our dumb component will display it.
  */
 @Component({
   selector:   'search-result',  // <selector-name></selector-name>
   directives:  [ ],             // Register child components.
-  template: ` <div *ngFor="#item of dataItems | async">
-                {{ item.name }} ({{ item.popularity }})
-              </div>
+  template: `
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr *ngFor="#item of dataItems | async">
+        <td>{{ item.name }}</td>
+        <td>{{ item.popularity }}</td>
+      </tr>
+    </tbody>
+  </table>
   `
 })
 class SearchResult {
@@ -53,8 +78,6 @@ class SpotifySearchService {
   // A base url for spotify artist search.
   private baseUrl = 'https://api.spotify.com/v1/search?type=artist';
 
-  private dataObserver: Observer<Response>;
-
   // Inject Http service.
   constructor( private _http: Http ) { }
 
@@ -68,7 +91,7 @@ class SpotifySearchService {
   /**
    * Load items by performing search based on the specified search term.
    */
-  loadItems( searchTerm ) {
+  loadItems( searchTerm ): Observable<Object[]> {
     // GET, convert the response to json and extract required data from it.
     return this._http
       .get( this.buildUrl( searchTerm ) )
@@ -81,39 +104,57 @@ class SpotifySearchService {
   } // end loadItems
 }
 
+
 /**
- * The wrapper component of the spotify-search functionality.
+ * The smart component that controls the spotify-search functionality.
+ * Uses the HTTP service to get search results and apply any transformations to
+ * the data that we need.
+ * Bound to the searchEvent output property in the search-box sub-component.
  */
 @Component({
   selector: 'spotify-search-component',    // <selector-name></selector-name>
   directives: [ SearchBox, SearchResult ], // Register child components.
-  providers:  [ SpotifySearchService ],
+  providers:  [ SpotifySearchService ],    // Register services.
+  encapsulation: ViewEncapsulation.Native, // Set ShadowDOM.
+  styles: [ require('./spotify-search.component.css') ],
   template: `
-    <search-box (valueChanged)="search( $event )"></search-box>
-    <hr>
-    <search-result [dataItems]="data"></search-result>
-    `
+  <search-box (searchEvent)="onSearch( $event )"></search-box>
+  <hr>
+  <search-result [dataItems]="data"></search-result>
+  `
 })
 export class SpotifySearchComponent {
-  private data: Observable<Response>;
-  private dataObserver: Observer<Response>;
+
+  private data: Observable<Object[]>;
+
+  // Responsible for generating the values that will be emitted through the
+  // Observable. We need an explicit reference to it in order to push new data
+  // from HTTP requests, using next() method.
+  private _dataObserver: Observer<Object[]>;
 
   constructor( private _http: Http,
                private _search: SpotifySearchService ) {
-    this.data = new Observable( observer => this.dataObserver = observer );
+    this.data = new Observable(
+      ( observer ) => {
+        return this._dataObserver = observer;
+      });
   }
 
   /**
    * Perform GET request to spotify API for artist data.
-   * Invoked when user's input changes.
+   * Invoked when user's input changes which are captured by search-box.
    */
-  search( searchTerm: string ) {
+  onSearch( searchTerm: string ) {
     this._search
       .loadItems( searchTerm )
       .subscribe(
-        ( res: Response ) => { this.dataObserver.next( res ); },
+        ( newResult ) => {
+          // Push a new copy of result list to all Subscribers.
+          // Reject if _dataObserver does not exist.
+          return this._dataObserver ? this._dataObserver.next( newResult ) : null;
+        },
         error => console.log( 'Could not load artists' ),
         ()    => console.log( 'Done loading artists' )
-    );
+      );
   } // end search
 }
